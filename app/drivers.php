da56db9e5aa1de8d19a6e8a750ac824c46203b73
amperrj <37677259+amperrj@users.noreply.github.com>
2018-04-06 12:46:58 +0800

    Update drivers.php

--- app/drivers.php
+++ app/drivers.php
@@ -2,28 +2,41 @@
 <html lang="en">
 	<head>
 		<?php include_once("layouts/dashboard.header.php") ?>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body class="login-page">
-		<?php include_once("layouts/dashboard.navigation.php") ?>
+		<!--<?php include_once("layouts/dashboard.navigation.php") ?>-->
 	    <div class="container-fluid">
 	      <div class="row">
 	      	<?php include_once("layouts/dashboard.sidebar.php") ?>
 	        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-						<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2">
-							<h1 class="h2"><span class="fa fa-fw fa-users"></span> Drivers</h1>
-								<div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
-								  <div class="btn-group mr-2" role="group" aria-label="First group">
-								    <button type="button" class="btn btn-success"><span class="fas fa-user-plus"></span></button>
-								    <button type="button" class="btn btn-danger"><span class="fas fa-user-times"></span></button>
-								  </div>
+					<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2">
+						<h1 class="h2"><span class="fa fa-fw fa-users"></span> Drivers</h1>
+						<p>
+						<a href="register.php"><button type="button" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Add</button></a>
+						<button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-remove-sign"></span> Delete</button>
+						<button type="button" class="btn btn-secondary"><span class="glyphicon glyphicon-pencil"></span> Update</button>
+						
+						</p>	
+						<p>
+							<div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+								 
 									<div class="input-group">
 									  <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2">
-									  <div class="input-group-append">
-											<div class="input-group-text btn-success"><i class="fa fa-lg fa-search"></i></div>
+									 <div class="input-group-append">
+											<div class="input-group-text btn-success"><i class="fa fa-lg fa-search"></i>
+											<button type="button" class="btn btn-info"> <span class="glyphicon glyphicon-search"></span> Search </button>
 										</div> 
+											</div>
+											
 									</div>
 								</div>
 						</div>
+						</p>
 						<div class="table-responsive">
 							<table class="table table-striped table-md">
 								<thead>
@@ -37,7 +50,7 @@
 									</tr>
 								</thead>
 								<tbody>
-									<tr>
+								<tr>
 										<td><input class="form-control" type="checkbox" name=""></td>
 										<td>1</td>
 										<td>DRV-0001</td>
@@ -50,7 +63,7 @@
 										<td>2</td>
 										<td>DRV-0002</td>
 										<td><a><img src="assets/images/upoulogo.png" alt="..." class="img-profile-sm img-thumbnail rounded-circle">Lorem, Ipsum Dolor</a></td>
-										<td><span class="btn-secondary btn-sm" href="#" role="button">Blocked</span></td>
+										<td><span class="btn-secondary btn-sm" href="#" role="button">Block</span></td>
 										<td>15 Sept, 8:56 AM (2015)</td>
 									</tr>
 									<tr>
@@ -66,8 +79,8 @@
 										<td>4</td>
 										<td>DRV-0004</td>
 										<td><a><img src="assets/images/upoulogo.png" alt="..." class="img-profile-sm img-thumbnail rounded-circle">Lorem, Ipsum Dolor</a></td>
-										<td><span class="btn-secondary btn-sm" href="#" role="button">Blocked 24h</span></td>
-										<td>15 Sept, 8:56 AM (2015)</td>
+										<td><span class="btn-secondary btn-sm" href="#" role="button">Block</span></td>
+									<td>15 Sept, 8:56 AM (2015)</td>
 									</tr>
 									<tr>
 										<td><input class="form-control" type="checkbox" name=""></td>
@@ -137,4 +150,4 @@
 	      </div>
 	    </div>
 	</body>
-</html>
+</html> 
